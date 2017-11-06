91a48b6755815de0388fb43f6488d19d0ef42212
50616017 <50616017@50616017-pc>
2017-11-06 17:43:59 +0900

    matrix2

--- src/matrix/Matrix_lib.java
+++ src/matrix/Matrix_lib.java
@@ -1,14 +1,17 @@
 package matrix;
 
+import java.util.Arrays;
+
 public class Matrix_lib {
-	public Matrix_lib(double[][] a, double[][] b) {
-		super();
-		this.a = a;
-		this.b = b;
-	}
-	private double a[][],b[][];
+	//private double a[][],b[][];
+
+	//public Matrix_lib(double[][] a, double[][] b) {
+		//super();
+		//this.a = a;
+		//this.b = b;
+	//}
 	
-	public double getinnerProduct(double a[],double b[]) {
+	public double getinnerProduct(double a[],double b[]) { //内積
 	double answer = 0;
 	for(int i = 0;i < a.length;i++) {
 		answer += a[i]*b[i];
@@ -17,7 +20,7 @@ public class Matrix_lib {
 	
 	}
 	
-	public double [][] getProduct() {
+	public double [][] getProduct(double a[][],double b[][]) {  //行列積
 		
 		double answer [][] = new double [a.length][b[0].length];
 	    double t[][] = this.getTranspose(b);
@@ -30,7 +33,7 @@ public class Matrix_lib {
 		
 		return answer;
 	}
-	public double [][] getTranspose (double a [][] ){
+	public double [][] getTranspose (double a [][] ){  //転置行列
 		double t[][] = new double [a[0].length][a.length];
 			for(int i = 0;i < a[0].length;i++) {
 				for(int j = 0; j < a.length;j++) {
@@ -40,6 +43,59 @@ public class Matrix_lib {
 		return t;
 	}
 	
+	public double getCofactor(double [][]a) {
+		double cofactor = 0;
+		if(a.length == 2 ){
+			cofactor = a[0][0]*a[1][1] - a[0][1]*a[1][0];			
+		}
+		else {
+			double tmp[][] = new double[a.length - 1][a[0].length - 1];
+			//1列について展開
+			int index = 0;
+			for(int i = 0; i < a.length; i++) {
+				//余因子行列作成
+				int p = 0,q = 0;
+				for(int j = 0; j < a.length; j++) {
+					if(i == j) continue;
+					for(int k = 0; k < a[0].length; k++) {
+						if(index == k) continue;
+						tmp[p][q++] = a[j][k];
+					}
+					p++;
+					q = 0;
+				}
+				System.out.println("tmp["+i+"] = " + Arrays.deepToString(tmp)); //確認のため表示する用
+				cofactor += a[i][index]*Math.pow(-1,(i + 1) + (index + 1))*this.getCofactor(tmp); 
+			}
+		}
+			
+		return cofactor;
+	}
+	
+	public double [][] getInverse(double[][]a){
+		double answer [][] = new double[a.length][a[0].length];
+		double tmp[][] = new double[a.length - 1][a[0].length - 1];
+		double determinant = this.getCofactor(a);
+		
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j < a[0].length; j++) {
+				int p = 0,q = 0;
+				for(int k = 0; k < a.length; k++) {
+					if(i == k) continue;
+					for(int s = 0; s < a[0].length; s++) {
+						if(j == s) continue;
+						tmp[p][q] = a[k][s];
+						q++;
+					}
+					p++;
+					q = 0;
+				}
+				answer[i][j] = Math.pow(-1, i + 1 + j + 1) * this.getCofactor(tmp) / determinant;
+			}
+		}
+		answer = this.getTranspose(answer);
+		return answer;
+	}
 	
 
 }
